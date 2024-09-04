246788c70fff2c75c906385dea3b6682de19eda7
wiscod <djihouae@gmail.com>
2024-09-04 23:19:23 +0200

    first commit

---
+++ src/main/java/com/wiscod/usersmanagementsystem/UsersManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wiscod.usersmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wiscod/usersmanagementsystem/repository/UsersRepo.java
@@ -0,0 +1,11 @@
+package com.wiscod.usersmanagementsystem.repository;
+
+import com.wiscod.usersmanagementsystem.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
